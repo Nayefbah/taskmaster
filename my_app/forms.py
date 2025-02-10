d471a65aa5be3dd1b5659b5b6b84a9af367112a4
Nayefbah <nayef.rind@gmail.com>
2025-02-10 09:39:30 +0300

    update notes and create task style

--- my_app/forms.py
+++ my_app/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import Job, Notes
 
-
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
@@ -10,10 +9,14 @@ class JobForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'status': forms.Select(attrs={'class': 'form-select'}),
-            'due_date': forms.TextInput(attrs={'class': 'form-control', 'id': 'id_due_date'}),
+            'due_date': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
 class NoteForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ['title', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
